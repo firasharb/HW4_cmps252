bb8be6adcf4a458040bacbf3e66932dd57e8cefb
firasharb <firas-harb@hotmail.com>
2021-11-13 17:13:22 +0200

    Implemented roomSchedule() method in CourSeera.java && implemented CourSeeraFactory.java

--- src/CourSeeraFactory.java
+++ src/CourSeeraFactory.java
@@ -3,3 +3,10 @@ import java.util.List;
 public interface CourSeeraFactory {
 	CourSeera createInstance(List<Course> courses);
 }
+
+
+class CourSeeraFactoryClass implements CourSeeraFactory { 
+	public CourSeera createInstance(List<Course> courses) {
+		return new CourSeeraClass(courses); 
+	}
+}
