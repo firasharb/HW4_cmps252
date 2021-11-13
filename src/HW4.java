a1db0de9d3f6b0691d5dfdb8cd7e79c7021ae64a
firasharb <firas-harb@hotmail.com>
2021-11-13 21:16:56 +0200

    Copied files from class file to interface file

--- src/HW4.java
+++ src/HW4.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -8,10 +9,23 @@ public class HW4 {
 		var d = LocalDate.parse("1999-01-22");
 		
 		System.out.println(t1);
-		Downloader d1 = new DownloaderClass();
-		d1.downloadHtmlToFile("C", "C.htm");
 		
-		HtmlToCsvClass c = new HtmlToCsvClass();
-		c.htmlToCsv("C.htm", "C.csv");
+		boolean check = new File("./", "A.htm").exists();
+		if(!check) { 
+			for(int i = 0; i < 26; i++) { 
+				char letter = (char) (65 + i);
+				Downloader d1 = new DownloaderClass();
+				d1.downloadHtmlToFile(letter + "", letter + ".htm");
+				
+				HtmlToCsv c = new HtmlToCsvClass();
+				c.htmlToCsv(letter + ".htm", letter + ".csv");
+			}
+		}
+		else { 
+			System.out.println("File already exists!");
+			
+			LocalTime t = LocalTime.of(18, 00);
+			System.out.print(t);
+		}
 	}
 }
