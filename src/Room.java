dc7c8f18a486e5609a84bf56810a1ddeaf3ab6e1
firasharb <firas-harb@hotmail.com>
2021-11-13 12:33:31 +0200

    Implemented Room and Instructor interfaces

--- src/Room.java
+++ src/Room.java
@@ -3,3 +3,24 @@ public interface Room {
 	String getBuilding();
 	String getRoomNumber();
 }
+
+class RoomClass implements Room {
+	private final String bldg; 
+	private final String roomNumber; 
+	
+	RoomClass(String bldg, String roomNumber) {
+		this.bldg = bldg; 
+		this.roomNumber = roomNumber;
+	}
+
+	@Override
+	public String getBuilding() {
+		return this.bldg;
+	}
+
+	@Override
+	public String getRoomNumber() {
+		return this.roomNumber;
+	} 
+	
+}
